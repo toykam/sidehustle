e6444353039f6896e5ed80d3caffe6368d1cf8b2
toykam <mwork063@gmail.com>
2021-06-22 15:45:27 +0100

    updated my code

--- assignment_1/assignment_1.dart
+++ assignment_1/assignment_1.dart
@@ -1,16 +1,26 @@
-
+import 'dart:io';
 
 main() {
-  num studentPercentage = 50;
-  num studentScore = 70;
-  num classAverage = 60;
+
+  print("Enter student percentage: ");
+  var studentPerc= stdin.readLineSync();
+
+  print("Enter student score: ");
+  var studentSc = stdin.readLineSync();
+
+  print("Enter class average: ");
+  var classAvg = stdin.readLineSync();
+
+  num studentPercentage = num.tryParse(studentPerc as String) as num;
+  num studentScore = num.tryParse(studentSc as String) as num;
+  num classAverage = num.tryParse(classAvg as String) as num;
 
 
   if (studentPercentage >= 60) {
     if (studentScore < (classAverage - 5)) {
-      print('Student passed this semester');
-    } else {
       print('Student failed this semester based on class average');
+    } else {
+      print('Student passed this semester');
     }
   } else {
     print('Student failed this semester based on general general percentage');
